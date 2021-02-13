a61d28fd3d590312c0b7065c27cef0a1a274b70e
Nikita <kupora3@gmail.com>
2021-02-13 12:41:44 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String first = "Hello world";
+        System.out.println(first);
+        int[] res = new int[]{1,2,3,4,5};
+    }
+}
