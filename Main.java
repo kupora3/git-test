27147a03a963acd14c29a5bc669cfc547af7403d
Nikita <kupora3@gmail.com>
2021-02-13 14:23:35 +0200

    for MA

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	String first = "Hello world";
+	String first = "Hello mates";
         System.out.println(first);
     }
 }
--- readme.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        String res = "If this work i'll be happy";
-                System.out.println(res);
-    }
-}
