58dd8626b2e4c89ae53aab56527b57dab47b617a
kupora3 <78849236+kupora3@users.noreply.github.com>
2021-02-13 14:30:13 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	String first = "Hello mates";
+	String first = "Hello mates!";
         System.out.println(first);
     }
 }
