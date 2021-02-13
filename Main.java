e6334894823c41545a38a9dae1cea3fcc6b6aace
kupora3 <78849236+kupora3@users.noreply.github.com>
2021-02-13 14:11:09 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	String first = "Hello world";
         System.out.println(first);
-        int[] res = new int[]{1,2,3,4,5};
     }
 }
