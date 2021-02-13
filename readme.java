77a80a2acfcbb9ad460a45d62f9bcd4e0367b436
Nikita <kupora3@gmail.com>
2021-02-13 13:17:33 +0200

    reame

---
+++ readme.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String res = "If this work i'll be happy";
+                System.out.println(res);
+    }
+}
